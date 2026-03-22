f3a4a4c753125c2a4eb1408a2896a8cac9206c9c
Nara Benetts <nellyguerrero914@gmail.com>
2026-03-22 16:20:50 -0700

    stored procedures completo

--- GAME_SHOP_DB.sql
+++ GAME_SHOP_DB.sql
@@ -445,6 +445,8 @@ delimiter ;
 -- -----------Stored Procedures------------
 delimiter $$
 
+--  client
+#insert
 create procedure sp_insert_client(
     in p_name varchar(255),
     in p_address varchar(255),
@@ -455,6 +457,41 @@ begin
     insert into clients(name_, address, phone, birthday)
     values (p_name, p_address, p_phone, p_birthday);
 end$$
+
+#read
+create procedure sp_get_clients()
+begin
+    select * from clients;
+end$$
+
+#update
+create procedure sp_update_client(
+    in p_id int,
+    in p_name varchar(255),
+    in p_address varchar(255),
+    in p_phone varchar(255),
+    in p_birthday date
+)
+begin
+    update clients
+    set name_ = p_name,
+        address = p_address,
+        phone = p_phone,
+        birthday = p_birthday
+    where client_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_client(
+    in p_id int
+)
+begin
+    delete from clients
+    where client_id = p_id;
+end$$
+
+-- supplier
+#create
 create procedure sp_insert_supplier(
     in p_name varchar(255),
     in p_address varchar(255),
@@ -466,6 +503,40 @@ begin
     values (p_name, p_address, p_phone, p_email);
 end$$
 
+#read
+create procedure sp_get_suppliers()
+begin
+    select * from suppliers;
+end$$
+
+#update
+create procedure sp_update_supplier(
+    in p_id int,
+    in p_name varchar(255),
+    in p_address varchar(255),
+    in p_phone varchar(255),
+    in p_email varchar(255)
+)
+begin
+    update suppliers
+    set name_ = p_name,
+        address = p_address,
+        phone = p_phone,
+        email = p_email
+    where supplier_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_supplier(
+    in p_id int
+)
+begin
+    delete from suppliers
+    where supplier_id = p_id;
+end$$
+
+-- branch
+#insert
 create procedure sp_insert_branch(
     in p_name varchar(255),
     in p_address varchar(255),
@@ -476,9 +547,41 @@ begin
     insert into branches(name_, address, phone, email)
     values (p_name, p_address, p_phone, p_email);
 end$$
---  pendientes  -- 
+
+#read
+create procedure sp_get_branches()
+begin
+    select * from branches;
+end$$
+
+#update
+create procedure sp_update_branch(
+    in p_id int,
+    in p_name varchar(255),
+    in p_address varchar(255),
+    in p_phone varchar(255),
+    in p_email varchar(255)
+)
+begin
+    update branches
+    set name_ = p_name,
+        address = p_address,
+        phone = p_phone,
+        email = p_email
+    where branch_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_branch(
+    in p_id int
+)
+begin
+    delete from branches
+    where branch_id = p_id;
+end$$
 
 -- departments
+#insert
 create procedure sp_insert_department(
     in p_des varchar(255),
     in p_branch_id int
@@ -488,15 +591,467 @@ begin
     values (p_des, p_branch_id);
 end$$
 
+#read
+create procedure sp_get_departments()
+begin
+    select * from departments;
+end$$
+
+#update
+create procedure sp_update_department(
+    in p_id int,
+    in p_des varchar(255),
+    in p_branch_id int
+)
+begin
+    update departments
+    set des = p_des,
+        branch_id = p_branch_id
+    where department_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_department(
+    in p_id int
+)
+begin
+    delete from departments
+    where department_id = p_id;
+end$$
+
 -- roles
+#insert
+create procedure sp_insert_role(
+    in p_name enum('manager','cashier','inventory_clerk','support')
+)
+begin
+    insert into roles(name_)
+    values (p_name);
+end$$
+
+#read
+create procedure sp_get_roles()
+begin
+    select * from roles;
+end$$
+
+#update
+create procedure sp_update_role(
+    in p_id int,
+    in p_name enum('manager','cashier','inventory_clerk','support')
+)
+begin
+    update roles
+    set name_ = p_name
+    where role_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_role(
+    in p_id int
+)
+begin
+    delete from roles
+    where role_id = p_id;
+end$$
+
 -- employees
+#insert
+create procedure sp_insert_employee(
+    in p_name varchar(255),
+    in p_address varchar(255),
+    in p_phone varchar(255),
+    in p_email varchar(255),
+    in p_department_id int,
+    in p_role_id int,
+    in p_birthday date,
+    in p_contract_day date
+)
+begin
+    insert into employees(name_, address, phone, email, department_id, role_id, birthday, contract_day)
+    values (p_name, p_address, p_phone, p_email, p_department_id, p_role_id, p_birthday, p_contract_day);
+end$$
+#read
+create procedure sp_get_employees()
+begin
+    select * from employees;
+end$$
+
+#update
+create procedure sp_update_employee(
+    in p_id int,
+    in p_name varchar(255),
+    in p_address varchar(255),
+    in p_phone varchar(255),
+    in p_email varchar(255),
+    in p_department_id int,
+    in p_role_id int,
+    in p_birthday date,
+    in p_contract_day date
+)
+begin
+    update employees
+    set name_ = p_name,
+        address = p_address,
+        phone = p_phone,
+        email = p_email,
+        department_id = p_department_id,
+        role_id = p_role_id,
+        birthday = p_birthday,
+        contract_day = p_contract_day
+    where employee_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_employee(
+    in p_id int
+)
+begin
+    delete from employees
+    where employee_id = p_id;
+end$$
+
 -- consoles
+#insert
+create procedure sp_insert_console(
+    in p_name varchar(255)
+)
+begin
+    insert into consoles(name_)
+    values (p_name);
+end$$
+
+#read
+create procedure sp_get_consoles()
+begin
+    select * from consoles;
+end$$
+
+#update
+create procedure sp_update_console(
+    in p_id int,
+    in p_name varchar(255)
+)
+begin
+    update consoles
+    set name_ = p_name
+    where console_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_console(
+    in p_id int
+)
+begin
+    delete from consoles
+    where console_id = p_id;
+end$$
+
 -- clasifications 
+#insert
+create procedure sp_insert_clasification(
+    in p_des enum('EC','E','E10+','T','M','AO','RP')
+)
+begin
+    insert into clasifications(des)
+    values (p_des);
+end$$
+
+#read
+create procedure sp_get_clasifications()
+begin
+    select * from clasifications;
+end$$
+
+#update
+create procedure sp_update_clasification(
+    in p_id int,
+    in p_des enum('EC','E','E10+','T','M','AO','RP')
+)
+begin
+    update clasifications
+    set des = p_des
+    where clasification_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_clasification(
+    in p_id int
+)
+begin
+    delete from clasifications
+    where clasification_id = p_id;
+end$$
+
 -- games 
+#insert
+create procedure sp_insert_game(
+    in p_name varchar(255),
+    in p_des varchar(255),
+    in p_developer varchar(255),
+    in p_clasification_id int,
+    in p_console_id int
+)
+begin
+    insert into games(name_, des, developer, clasification_id, console_id)
+    values (p_name, p_des, p_developer, p_clasification_id, p_console_id);
+end$$
+
+#read
+create procedure sp_get_games()
+begin
+    select * from games;
+end$$
+ 
+#update
+create procedure sp_update_game(
+    in p_id int,
+    in p_name varchar(255),
+    in p_des varchar(255),
+    in p_developer varchar(255),
+    in p_clasification_id int,
+    in p_console_id int
+)
+begin
+    update games
+    set name_ = p_name,
+        des = p_des,
+        developer = p_developer,
+        clasification_id = p_clasification_id,
+        console_id = p_console_id
+    where game_id = p_id;
+end$$
+ 
+#delete
+create procedure sp_delete_game(
+    in p_id int
+)
+begin
+    delete from games
+    where game_id = p_id;
+end$$
+ 
 -- products
+#insert
+create procedure sp_insert_product(
+    in p_type enum('game','console','accessory'),
+    in p_name varchar(255),
+    in p_des varchar(255),
+    in p_price float,
+    in p_console_id int,
+    in p_game_id int
+)
+begin
+    insert into products(product_type, name_, des, price, console_id, game_id)
+    values (p_type, p_name, p_des, p_price, p_console_id, p_game_id);
+end$$
+
+#read
+create procedure sp_get_products()
+begin
+    select * from products;
+end$$
+
+#update
+create procedure sp_update_product(
+    in p_id int,
+    in p_price float
+)
+begin
+    update products
+    set price = p_price
+    where product_id = p_id;
+end$$
+
+#delete
+create procedure sp_delete_product(
+    in p_id int
+)
+begin
+    delete from products
+    where product_id = p_id;
+end$$
+
 -- inventory
+#insert
+create procedure sp_insert_inventory(
+    in p_product_id int,
+    in p_stock int,
+    in p_supplier_id int,
+    in p_branch_id int
+)
+begin
+    insert into inventory(product_id, stock, supplier_id, branch_id)
+    values (p_product_id, p_stock, p_supplier_id, p_branch_id);
+end$$
+ 
+#read
+create procedure sp_get_inventory()
+begin
+    select * from inventory;
+end$$
+ 
+#update
+create procedure sp_update_inventory(
+    in p_id int,
+    in p_product_id int,
+    in p_stock int,
+    in p_supplier_id int,
+    in p_branch_id int
+)
+begin
+    update inventory
+    set product_id = p_product_id,
+        stock = p_stock,
+        supplier_id = p_supplier_id,
+        branch_id = p_branch_id
+    where inventory_id = p_id;
+end$$
+ 
+#delete
+create procedure sp_delete_inventory(
+    in p_id int
+)
+begin
+    delete from inventory
+    where inventory_id = p_id;
+end$$
+
 -- paymethods
+#insert
+create procedure sp_insert_paymethod(
+    in p_des enum('cash','credit_card','debit_card','transfer')
+)
+begin
+    insert into paymethods(des)
+    values (p_des);
+end$$
+ 
+#read
+create procedure sp_get_paymethods()
+begin
+    select * from paymethods;
+end$$
+ 
+#update
+create procedure sp_update_paymethod(
+    in p_id int,
+    in p_des enum('cash','credit_card','debit_card','transfer')
+)
+begin
+    update paymethods
+    set des = p_des
+    where paymethod_id = p_id;
+end$$
+ 
+#delete
+create procedure sp_delete_paymethod(
+    in p_id int
+)
+begin
+    delete from paymethods
+    where paymethod_id = p_id;
+end$$
+
 -- sales_order
+#insert
+create procedure sp_insert_sales_order(
+    in p_employee_id int,
+    in p_client_id int,
+    in p_paymethod_id int,
+    in p_date date,
+    in p_total float
+)
+begin
+    insert into sales_order(employee_id, client_id, paymethod_id, date_, total)
+    values (p_employee_id, p_client_id, p_paymethod_id, p_date, p_total);
+end$$
+ 
+#read
+create procedure sp_get_sales_orders()
+begin
+    select * from sales_order;
+end$$
+ 
+#update
+create procedure sp_update_sales_order(
+    in p_id int,
+    in p_employee_id int,
+    in p_client_id int,
+    in p_paymethod_id int,
+    in p_date date,
+    in p_total float
+)
+begin
+    update sales_order
+    set employee_id = p_employee_id,
+        client_id = p_client_id,
+        paymethod_id = p_paymethod_id,
+        date_ = p_date,
+        total = p_total
+    where sale_id = p_id;
+end$$
+ 
+#delete
+create procedure sp_delete_sales_order(
+    in p_id int
+)
+begin
+    delete from sales_order
+    where sale_id = p_id;
+end$$
+
 -- order_details
+#insert
+create procedure sp_insert_order_detail(
+    in p_sale_id int,
+    in p_product_id int,
+    in p_quantity int,
+    in p_unit_price float
+)
+begin
+    insert into order_details(sale_id, product_id, quantity, unit_price)
+    values (p_sale_id, p_product_id, p_quantity, p_unit_price);
+end$$
+ 
+#read
+create procedure sp_get_order_details()
+begin
+    select * from order_details;
+end$$
+ 
+#update
+create procedure sp_update_order_detail(
+    in p_id int,
+    in p_sale_id int,
+    in p_product_id int,
+    in p_quantity int,
+    in p_unit_price float
+)
+begin
+    update order_details
+    set sale_id = p_sale_id,
+        product_id = p_product_id,
+        quantity = p_quantity,
+        unit_price = p_unit_price
+    where order_detail_id = p_id;
+end$$
+ 
+#delete
+create procedure sp_delete_order_detail(
+    in p_id int
+)
+begin
+    delete from order_details
+    where order_detail_id = p_id;
+end$$
+
+delimiter ;
+
+-- -----------------Inserts-------------------
+
+
+
+
 
 
